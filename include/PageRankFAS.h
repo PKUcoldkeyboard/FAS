e7232fdfbdfba1ebf710ed5c0496da801c7e6701
cuterwrite <158505862@qq.com>
2023-03-28 23:02:15 +0800

    update PageRankFAS.h

--- include/PageRankFAS.h
+++ include/PageRankFAS.h
@@ -12,7 +12,8 @@ public:
 private:
     Graph* getLineGraph(Graph &g);
     std::vector<double> calculatePageRank(Graph &lingGraph);
-    std::vector<int> connected_components(Graph &g);
+    std::vector<Graph> connected_components(Graph &g);
+    bool has_circle(Graph &g);
 };
 
 #endif // __PAGERANK_H__
