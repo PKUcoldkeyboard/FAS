26160fcf3784077f2e2444e35262b7fed6836b06
putao537 <putao537@gmail.com>
2023-03-29 10:41:30 +0800

    Fix conflict

--- include/PageRankFAS.h
+++ include/PageRankFAS.h
@@ -12,13 +12,9 @@ public:
 private:
     Graph* getLineGraph(Graph &g);
     std::vector<double> calculatePageRank(Graph &lingGraph);
-<<<<<<< HEAD
+
     std::vector<std::vector<int>> getConnectedComponents(Graph &g);
     bool isCyclic(Graph &g);
-=======
-    std::vector<std::vector<int>> getConnectedComponents(Graph &g);
-    bool isCyclic(Graph &g);
->>>>>>> 63eae86c18a96c0dd608f9f35a91430bb212ac33
 };
 
 #endif // __PAGERANK_H__
