a8e3c6d09755bc45a9ee42bb20145d545c281399
cuterwrite <158505862@qq.com>
2023-04-02 17:48:11 +0800

    修改测试样例，更新大图的测试通过标准

--- test/test.cpp
+++ test/test.cpp
@@ -12,7 +12,7 @@
 TEST(CycleDetectionTest, CycleDetectionTest1) {
     // 创建Graph
     Graph g(5);
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}};
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 0}, {3, 1}, {4, 5}, {5, 6}, {6, 4}};
 
     for (auto &edge : edges) {
         add_edge(edge.first, edge.second, g);
@@ -104,29 +104,25 @@ TEST(StrongConnectedComponentsTest, StrongConnectedComponentsTest3) {
 */
 TEST(MinFeedBackArcSetTest, GreedyFASTest1) {
     // 创建Graph
-    Graph g(5);
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}};
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 0}, {3, 1}, {4, 5}, {5, 6}, {6, 4}};
 
-    for (auto &edge : edges) {
-        add_edge(edge.first, edge.second, g);
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
     }
 
-    // 调用GreedyFAS获取最小反馈弧集
-    FASContext context("greedy");
-    auto feedback_arcs = context.getFeedbackArcSet(g);
-
-    ASSERT_EQ(feedback_arcs.size(), 1);
-    ASSERT_EQ(source(feedback_arcs[0], g), 4);
-    ASSERT_EQ(target(feedback_arcs[0], g), 0);
-}
-
-TEST(MinFeedBackArcSetTest, GreedyFASTest2) {
-    // 创建Graph
-    Graph g(5);
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1}};
-
-    for (auto &edge : edges) {
-        add_edge(edge.first, edge.second, g);
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
     }
 
     // 调用GreedyFAS获取最小反馈弧集
@@ -134,10 +130,116 @@ TEST(MinFeedBackArcSetTest, GreedyFASTest2) {
     auto feedback_arcs = context.getFeedbackArcSet(g);
 
     ASSERT_EQ(feedback_arcs.size(), 2);
-    // ASSERT_EQ(source(feedback_arcs[0], g), 4);
-    // ASSERT_EQ(target(feedback_arcs[0], g), 1);
-    // ASSERT_EQ(source(feedback_arcs[1], g), 3);
-    // ASSERT_EQ(target(feedback_arcs[1], g), 0);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, GreedyFASTest2) {
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1}};
+
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    // 调用GreedyFAS获取最小反馈弧集
+    FASContext context("greedy");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 3);
+    ASSERT_LE(feedback_arcs.size(), 5);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, GreedyFASTest3) {
+    // 严格测试，需要至少去掉6条边才能没有环的图
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
+                                   {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 5},
+                                   {5, 7}, {6, 8}, {7, 9}, {8, 5}, {9, 6}};
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用GreedyFAS获取最小反馈弧集
+    FASContext context("greedy");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 6);
+    ASSERT_LE(feedback_arcs.size(), 8);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, GreedyFASTest4) {
+    // 严格测试，需要至少去掉18条边才能没有环的图
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
+                                {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 5},
+                                {5, 7}, {6, 8}, {7, 9}, {8, 5}, {9, 6}, {0, 6}, {1, 7}, {2, 8}, {3, 9}, {4, 5},
+                                {5, 1}, {6, 2}, {7, 3}, {8, 4}, {9, 0}, {0, 8}, {1, 9}, {2, 5}, {3, 6}, {4, 7},
+                                {5, 3}, {6, 4}, {7, 0}, {8, 1}, {9, 2}};
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用GreedyFAS获取最小反馈弧集
+    FASContext context("greedy");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 15);
+    ASSERT_LE(feedback_arcs.size(), 23);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
 }
 
 /**
@@ -146,29 +248,25 @@ TEST(MinFeedBackArcSetTest, GreedyFASTest2) {
 
 TEST(MinFeedBackArcSetTest, SortFASTest1) {
     // 创建Graph
-    Graph g(5);
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}};
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 0}, {3, 1}, {4, 5}, {5, 6}, {6, 4}};
 
-    for (auto &edge : edges) {
-        add_edge(edge.first, edge.second, g);
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
     }
 
-    // 调用SortFAS获取最小反馈弧集
-    FASContext context("sort");
-    auto feedback_arcs = context.getFeedbackArcSet(g);
-
-    ASSERT_EQ(feedback_arcs.size(), 1);
-    // ASSERT_EQ(source(feedback_arcs[0], g), 4);
-    // ASSERT_EQ(target(feedback_arcs[0], g), 0);
-}
-
-TEST(MinFeedBackArcSetTest,SortFASTest2) {
-    // 创建Graph
-    Graph g(5);
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1}};
-
-    for (auto &edge : edges) {
-        add_edge(edge.first, edge.second, g);
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
     }
 
     // 调用SortFAS获取最小反馈弧集
@@ -176,22 +274,129 @@ TEST(MinFeedBackArcSetTest,SortFASTest2) {
     auto feedback_arcs = context.getFeedbackArcSet(g);
 
     ASSERT_EQ(feedback_arcs.size(), 2);
-    // ASSERT_EQ(source(feedback_arcs[0], g), 4);
-    // ASSERT_EQ(target(feedback_arcs[0], g), 1);
-    // ASSERT_EQ(source(feedback_arcs[1], g), 3);
-    // ASSERT_EQ(target(feedback_arcs[1], g), 0);
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest,SortFASTest2) {
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1}};
+
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用SortFAS获取最小反馈弧集
+    FASContext context("sort");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 3);
+    ASSERT_LE(feedback_arcs.size(), 5);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, SortFASTest3) {
+    // 严格测试，需要至少去掉6条边才能没有环的图
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
+                                   {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 5},
+                                   {5, 7}, {6, 8}, {7, 9}, {8, 5}, {9, 6}};
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用SortFAS获取最小反馈弧集
+    FASContext context("sort");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 6);
+    ASSERT_LE(feedback_arcs.size(), 8);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, SortFASTest4) {
+    // 严格测试，需要删除至少18条边才能使这个图没有环
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
+                                {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 5},
+                                {5, 7}, {6, 8}, {7, 9}, {8, 5}, {9, 6}, {0, 6}, {1, 7}, {2, 8}, {3, 9}, {4, 5},
+                                {5, 1}, {6, 2}, {7, 3}, {8, 4}, {9, 0}, {0, 8}, {1, 9}, {2, 5}, {3, 6}, {4, 7},
+                                {5, 3}, {6, 4}, {7, 0}, {8, 1}, {9, 2}};
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用SortFAS获取最小反馈弧集
+    FASContext context("sort");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 15);
+    ASSERT_LE(feedback_arcs.size(), 23);
+    
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
 }
 
 /**
  * PageRankFAS算法单元测试
 */
-/**
- * GreedyFAS算法单元测试
-*/
 TEST(MinFeedBackArcSetTest, PageRankFASTest1) {
     // 创建Graph
     Graph g;
-    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}};
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 0}, {3, 1}, {4, 5}, {5, 6}, {6, 4}};
 
     std::unordered_map<int, Vertex> vertex_map;
     for (const auto & edge: edges) {
@@ -215,7 +420,11 @@ TEST(MinFeedBackArcSetTest, PageRankFASTest1) {
     FASContext context("pagerank");
     auto feedback_arcs = context.getFeedbackArcSet(g);
 
-    ASSERT_EQ(feedback_arcs.size(), 1);
+    ASSERT_EQ(feedback_arcs.size(), 2);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
 }
 
 TEST(MinFeedBackArcSetTest, PageRankFASTest2) {
@@ -245,11 +454,16 @@ TEST(MinFeedBackArcSetTest, PageRankFASTest2) {
     FASContext context("pagerank");
     auto feedback_arcs = context.getFeedbackArcSet(g);
 
-    ASSERT_EQ(feedback_arcs.size(), 3);
+    ASSERT_GE(feedback_arcs.size(), 3);
+    ASSERT_LE(feedback_arcs.size(), 5);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
 }
 
 TEST(MinFeedBackArcSetTest, PageRankFASTest3) {
-    // 严格测试，需要至少去掉7条边才能没有环的图
+    // 严格测试，需要至少去掉6条边才能没有环的图
     // 创建Graph
     Graph g;
     std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
@@ -277,7 +491,51 @@ TEST(MinFeedBackArcSetTest, PageRankFASTest3) {
     FASContext context("pagerank");
     auto feedback_arcs = context.getFeedbackArcSet(g);
 
-    ASSERT_EQ(feedback_arcs.size(), 6);
+    ASSERT_GE(feedback_arcs.size(), 6);
+    ASSERT_LE(feedback_arcs.size(), 8);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
+}
+
+TEST(MinFeedBackArcSetTest, PageRankFASTest4) {
+    // 严格测试：需要删除至少15条边才能没有环
+    // 创建Graph
+    Graph g;
+    std::vector<EdgePair> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}, {0, 2}, {1, 3}, {2, 4}, {3, 0}, {4, 1},
+                                {0, 5}, {1, 6}, {2, 7}, {3, 8}, {4, 9}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 5},
+                                {5, 7}, {6, 8}, {7, 9}, {8, 5}, {9, 6}, {0, 6}, {1, 7}, {2, 8}, {3, 9}, {4, 5},
+                                {5, 1}, {6, 2}, {7, 3}, {8, 4}, {9, 0}, {0, 8}, {1, 9}, {2, 5}, {3, 6}, {4, 7},
+                                {5, 3}, {6, 4}, {7, 0}, {8, 1}, {9, 2}};
+    std::unordered_map<int, Vertex> vertex_map;
+    for (const auto & edge: edges) {
+        if (!vertex_map.count(edge.first)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.first;
+            vertex_map[edge.first] = v;
+        }
+        if (!vertex_map.count(edge.second)) {
+            auto v = add_vertex(g);
+            g[v].id = edge.second;
+            vertex_map[edge.second] = v;
+        }
+    }
+
+    for (const auto &edge : edges) {
+        add_edge(vertex_map[edge.first], vertex_map[edge.second], g);
+    }
+
+    // 调用PageRankFAS获取最小反馈弧集
+    FASContext context("pagerank");
+    auto feedback_arcs = context.getFeedbackArcSet(g);
+
+    ASSERT_GE(feedback_arcs.size(), 15);
+    ASSERT_LE(feedback_arcs.size(), 23);
+
+    // 检查是否有环
+    PageRankFAS fas;
+    ASSERT_FALSE(fas.isCyclic(g));
 }
 
 int main(int argc, char** argv) {
