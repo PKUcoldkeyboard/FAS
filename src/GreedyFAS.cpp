f4165edc398968002f6642b6cd19c73a16be783e
cuterwrite <158505862@qq.com>
2023-04-13 11:43:04 +0800

    fix bug of GreedyFAS

--- src/GreedyFAS.cpp
+++ src/GreedyFAS.cpp
@@ -1,139 +1,156 @@
 #include "GreedyFAS.h"
 #include <boost/graph/graph_utility.hpp>
+#include <numeric>
+#include <random>
 
-// 定义GreedyGraph数据类型
-typedef boost::adjacency_list<
-        boost::vecS,                                 // 边容器
-        boost::vecS,                                 // 节点容器
-        boost::bidirectionalS,                       // 双向有向图
-        int,                                         // 节点属性： 实际存储节点的id
-        boost::no_property                           // 边属性： 无
-> GreedyGraph;
-
-constexpr Vertex INVALID_VERTEX = std::numeric_limits<Vertex>::max();
-
-boost::container::vector<Vertex> get_source(const GreedyGraph &g) {
-    boost::container::vector<Vertex> ret;
-    for (const auto &v : boost::make_iterator_range(boost::vertices(g))) {
-        if (boost::in_degree(v, g) == 0) {
-            ret.emplace_back(v);
-        }
+// 定义GreedyGraph数据类型，用链式前向星
+class GreedyGraph {
+public:
+    int n;
+    struct Edge {
+        int to;
+        int next;
+    };
+    GreedyGraph(int n) : n(n) {
+        head.resize(n, -1);
+        out_degree.resize(n, 0);
     }
-    return ret;
+    boost::container::vector<Edge> edges;
+    boost::container::vector<int> head;
+    boost::container::vector<int> out_degree;
+
+    void add_edge(int u, int v) {
+        edges.emplace_back(Edge{v, head[u]});
+        head[u] = edges.size() - 1;
+        out_degree[u]++;
+    }
+};
+
+void update_degree(GreedyGraph &g, GreedyGraph &inverse, const int &u) {
+    // 出边的入度减1
+    for (int i = g.head[u]; i != -1; i = g.edges[i].next) {
+        int v = g.edges[i].to;
+        inverse.out_degree[v]--;
+    }
+    // 入边的出度减1
+    for (int i = inverse.head[u]; i != -1; i = inverse.edges[i].next) {
+        int v = inverse.edges[i].to;
+        g.out_degree[v]--;
+    }
 }
 
-boost::container::vector<Vertex> get_sink(const GreedyGraph &g) {
-    boost::container::vector<Vertex> ret;
-    for (const auto &v : boost::make_iterator_range(boost::vertices(g))) {
-        if (boost::out_degree(v, g) == 0) {
-            ret.emplace_back(v);
+void get_sink(GreedyGraph &g, boost::container::vector<int> &sink, const boost::container::vector<int> &state) {
+    for (int i = 0; i < g.n; i++) {
+        if (g.head[i] == -1) {
+            continue;
+        }
+        if (state[i] != 0) {
+            continue;
+        }
+        if (g.out_degree[i] == 0) {
+            sink.emplace_back(i);
         }
     }
-    return ret;
 }
 
-bool empty(const GreedyGraph &g) {
-    return boost::num_vertices(g) == 0;
+void get_source(GreedyGraph &inverse, boost::container::vector<int> &source, const boost::container::vector<int> &state) {
+    for (int i = 0; i < inverse.n; i++) {
+        if (inverse.head[i] == -1) {
+            continue;
+        }
+        if (state[i] != 0) {
+            continue;
+        }
+        if (inverse.out_degree[i] == 0) {
+            source.emplace_back(i);
+        }
+    }
 }
 
 boost::container::vector<EdgePair> GreedyFAS::getFeedbackArcSet(Graph &g) {
     SPDLOG_INFO("Starting GreedyFAS...");
 
-    GreedyGraph gg;
-    emhash7::HashMap<int, Vertex> vertex_map;
-
-    // 需要重新构图
-    for (const auto &e : boost::make_iterator_range(boost::edges(g))) {
-        const auto &src = boost::source(e, g);
-        const auto &tgt = boost::target(e, g);
-        const auto &src_id = g[src];
-        const auto &tgt_id = g[tgt];
-        if (!vertex_map.count(src_id)) {
-            auto v = add_vertex(src_id, gg);
-            vertex_map[src_id] = v;
-        }
-        if (!vertex_map.count(tgt_id)) {
-            auto v = add_vertex(tgt_id, gg);
-            vertex_map[tgt_id] = v;
-        }
+    // 重新存图
+    int n = 0;
+    // n等于g[v]的最大值
+    for (const auto &v : boost::make_iterator_range(boost::vertices(g))) {
+        n = std::max(n, g[v]);
     }
-
+    n++;
+    GreedyGraph graph(n), inverse(n);
     for (const auto &e : boost::make_iterator_range(boost::edges(g))) {
-        const auto &src = boost::source(e, g);
-        const auto &tgt = boost::target(e, g);
-        const auto &src_id = g[src];
-        const auto &tgt_id = g[tgt];
-        boost::add_edge(vertex_map[src_id], vertex_map[tgt_id], gg);
+        int u = boost::source(e, g);
+        int v = boost::target(e, g);
+        graph.add_edge(g[u], g[v]);
+        inverse.add_edge(g[v], g[u]);
     }
-
+    boost::container::vector<EdgePair> result;
     auto start = std::chrono::high_resolution_clock::now();
 
+    // 初始化状态
+    boost::container::vector<int> state(n + 1, 0);
     boost::container::vector<int> s1, s2;
-    boost::container::vector<EdgePair> result;
 
-    srand(time(nullptr));
-
-    while (!empty(gg)) {
+    while (std::accumulate(state.begin(), state.end(), 0) != n) {
         while (true) {
-            const auto &source = get_source(gg);
-            if (source.empty()) {
-                break;
-            }
-            int idx = rand() % source.size();
-            s2.insert(s2.begin(), gg[source[idx]]);
-            boost::clear_vertex(idx, gg);
-            boost::remove_vertex(idx, gg);
-        }
-
-        while (true) {
-            const auto &sink = get_sink(gg);
+            boost::container::vector<int> sink;
+            get_sink(graph, sink, state);
             if (sink.empty()) {
                 break;
             }
+            srand(time(nullptr));
             int idx = rand() % sink.size();
-            s1.emplace_back(gg[sink[idx]]);
-            boost::clear_vertex(idx, gg);
-            boost::remove_vertex(idx, gg);
+            const auto &v = sink[idx];
+            state[v] = 1;
+            s2.insert(s2.begin(), v);
+            update_degree(graph, inverse, v);
         }
-
-        int max_delta = -1;
-        Vertex u = INVALID_VERTEX;
-        for (const auto &v : boost::make_iterator_range(boost::vertices(gg))) {
-            int delta = boost::out_degree(v, gg) - boost::in_degree(v, gg);
-            if (delta > max_delta) {
-                max_delta = delta;
-                u = v;
+        while (true) {
+            boost::container::vector<int> source;
+            get_source(inverse, source, state);
+            if (source.empty()) {
+                break;
             }
+            srand(time(nullptr));
+            int idx = rand() % source.size();
+            const auto &u = source[idx];
+            state[u] = 1;
+            s1.emplace_back(u);
+            update_degree(graph, inverse, u);
         }
-        if (u != INVALID_VERTEX && !empty(gg)) {
-            s1.push_back(gg[u]);
-            boost::clear_vertex(u, gg);
-            boost::remove_vertex(u, gg);
+        if (std::accumulate(state.begin(), state.end(), 0) != n) {
+            int max_delta = std::numeric_limits<int>::min();
+            int u = -1;
+            for (int i = 0; i < n; i++) {
+                if (state[i] != 0) {
+                    continue;
+                }
+                int delta = graph.out_degree[i] - inverse.out_degree[i];
+                if (delta > max_delta) {
+                    max_delta = delta;
+                    u = i;
+                }
+            }
+            if (u != -1) {
+                s1.push_back(u);
+                state[u] = 1;
+                update_degree(graph, inverse, {u});
+            }
         }
     }
     s1.insert(s1.end(), s2.begin(), s2.end());
 
     for (int i = 0; i < s1.size() - 1; i++) {
-        int v = s1[i];
-        const auto &vv = vertex_map.find(v);
-        if (vv == vertex_map.end()) {
-            continue;
-        }
+        int u = s1[i];
         for (int j = i + 1; j < s1.size(); j++) {
-            int w = s1[j];
-            const auto &ww = vertex_map.find(w);
-            if (ww == vertex_map.end()) {
-                continue;
-            }
-            for (const auto &e : boost::make_iterator_range(boost::out_edges(ww->second, g))) {
-                const auto &tgt = boost::target(e, g);
-                if (tgt == vv->second) {
-                    result.emplace_back(w, v);
+            int v = s1[j];
+            for (int k = graph.head[v]; k != -1; k = graph.edges[k].next) {
+                if (graph.edges[k].to == u) {
+                    result.emplace_back(v, u);
                 }
             }
         }
     }
-
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     SPDLOG_INFO("Successfully compute FAS, FAS number: {}, Time Elapsed: {}s", result.size(), duration / 1000.0);
